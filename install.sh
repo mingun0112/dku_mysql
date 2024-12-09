cdd585a94cf8e2942b805461af6da6faf7aa6b73
Jeong mingyun <mingun0112@naver.com>
2024-12-09 16:09:12 +0900

    add install

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. 시스템 패키지 업데이트
+echo "Updating system packages..."
+sudo apt update -y
+
+# 2. Python과 pip 설치
+echo "Installing Python and pip..."
+sudo apt install python3 python3-pip -y
+sudo apt-get install mysql-server
+
+echo "Installing required Python packages..."
+pip3 install pymysql
